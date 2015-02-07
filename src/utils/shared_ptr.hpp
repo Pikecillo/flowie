e4a97e4600b7dceb451d6c98d8b6f45e581620ec
mario <mario.rincon.nigro@gmail.com>
2015-02-07 04:21:38 -0600

    GPL v3 License

--- src/utils/shared_ptr.hpp
+++ src/utils/shared_ptr.hpp
@@ -1,3 +1,22 @@
+/**
+ * Copyright (C) 2014. Mario Rincon-Nigro.
+ *
+ * This file is a part of Flowie.
+ *
+ * Flowie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Flowie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Flowie.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __SHARED_PTR_HPP__
 #define __SHARED_PTR_HPP__
 
