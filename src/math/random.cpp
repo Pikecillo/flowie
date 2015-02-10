fd56faf636a80081410e0458218215c209e041f8
mario <mario.rincon.nigro@gmail.com>
2015-02-10 12:03:08 -0600

    Random functions

---
+++ src/math/random.cpp
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2015. Mario Rincon-Nigro.
+ *
+ * This file is a part of Flowie.
+ *
+ * Flowie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Flowie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Flowie.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <math/random.hpp>
+
+#include <stdlib.h>
+
+namespace flowie
+{
+
+std::vector<int> rand_range(int lo, int hi, int n) {
+    std::vector<int> elements;
+    
+    for(int i = 0; i < n; i++) {
+	int random = rand() % (hi - lo) + lo;
+	elements.push_back(random);
+    }
+
+    return elements;
+}
+
+}
+
